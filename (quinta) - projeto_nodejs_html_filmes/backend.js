4090209874ba8cec6f654e5580e9b378078dcf29
Evandro Ferraz <ferraz.evandro@outlook.com>
2024-10-19 17:12:03 -0300

    Add files via upload

---
+++ (quinta) - projeto_nodejs_html_filmes/backend.js
@@ -0,0 +1,60 @@
+// mongodb+srv://evandroferraz:1234@cluster0.vev20.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
+console.log("Hello, NodeJS")
+
+const express = require ('express')
+const app = express()
+app.use(express.json())
+
+const cors = require ('cors')
+app.use(cors())
+
+// carrega as funções do pacote 'mongoose' na constante
+const mongoose = require('mongoose')
+// cria um Schema para representar filmes
+const Filme = mongoose.model("Filme", mongoose.Schema({
+    titulo: {type:String},
+    sinopse: {type:String}
+}))
+// cria uma função para fazer a conexão com o MongoDB
+async function conectarAoMongoDB(){
+    await mongoose.connect("mongodb+srv://evandroferraz:1234@cluster0.vev20.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+}
+
+app.listen(3000, () => {
+    try{
+        conectarAoMongoDB()
+        console.log("up and running")
+    }catch(e){
+        console.log("Erro", e)
+    }
+})
+
+//GET http://localhost:3000/hey
+app.get('/hey', (req, res) => {
+    res.send('hey')
+})
+
+//GET http://localhost:3000/filmes
+app.get("/filmes", async (req, res) => {
+    const filmes = await Filme.find()
+    res.json(filmes)
+})
+
+//POST http://localhost:3000/filmes
+app.post("/filmes", async (req, res) => {
+    //obtém os dados enviados pelo cliente
+    const titulo = req.body.titulo
+    const sinopse = req.body.sinopse
+    //monta um objeto agrupando os dados. Ele representa um novo filme
+    const filme = new Filme({titulo: titulo, sinopse: sinopse})
+    //adiciona o novo que foi criado a parti do Schema de Filmes
+    // já no cluster do MongoDB
+    await filme.save()
+    // cria a constante que guarda a lista de filmes e carrega esta
+    // constante com a lista de filmes encontrada entre as coleções
+    // do cluster no mongodb
+    const filmes = await Filme.find()
+    //responde ao cliente. Aqui, optamos por devolver a base inteira ao cliente, embora não seja obrigatório.
+    res.json(filmes)
+})
